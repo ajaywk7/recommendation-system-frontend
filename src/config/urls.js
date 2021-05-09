35ebd1d948403415a38380f646f6b945472f1537
ajaywk7 <59398018+ajaywk7@users.noreply.github.com>
2021-05-09 12:27:39 +0530

    Update urls.js

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,3 +1,3 @@
 export const server = "http://127.0.0.1:5000/";
 export const googleClientId =
-  "960998837328-hep4j8i99agm1g4par16og37dq0mang2.apps.googleusercontent.com";
+  "<your google credential>";
